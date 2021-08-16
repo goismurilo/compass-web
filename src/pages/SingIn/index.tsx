1513f9f0a44427eb885c1254d47c401565931c5b
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-08-16 13:36:26 -0300

    change: Isolating Components

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -4,6 +4,9 @@ import { FiLogIn } from 'react-icons/fi';
 
 import logoImg from '../../assets/compass-orange.png';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => (
@@ -12,9 +15,9 @@ const SignIn: React.FC = () => (
             <img src={logoImg} alt="Logo Compass OS" />
             <form>
                 <h1>Faça seu logon</h1>
-                <input placeholder="Email" />
-                <input type="password" placeholder="Senha" />
-                <button type="submit">Entrar</button>
+                <Input name="email" placeholder="Email" />
+                <Input name="password" type="password" placeholder="Senha" />
+                <Button type="submit">Entrar</Button>
 
                 <a href="forgot">Esqueci minha senha</a>
             </form>
