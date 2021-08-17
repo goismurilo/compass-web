1b0e11274fd72a9853fc22a986eda8696fd2342d
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-08-16 21:43:16 -0300

    add: Create Unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/compass-orange.png';
 
@@ -9,29 +9,32 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SingUp: React.FC = () => (
-    <Container>
-        <Background />
-        <Content>
-            <img src={logoImg} alt="Logo Compass OS" />
-            <form>
-                <h1>Faça seu cadastro</h1>
-                <Input name="name" icon={FiUser} placeholder="Nome" />
-                <Input name="email" icon={FiMail} placeholder="Email" />
-                <Input
-                    name="password"
-                    icon={FiLock}
-                    type="password"
-                    placeholder="Senha"
-                />
-                <Button type="submit">Cadastrar</Button>
-            </form>
-            <a href="login">
-                <FiArrowLeft />
-                Voltar para logon
-            </a>
-        </Content>
-    </Container>
-);
+const SingUp: React.FC = () => {
+    function handleSubmit(data: object): void { }
+    return (
+        <Container>
+            <Background />
+            <Content>
+                <img src={logoImg} alt="Logo Compass OS" />
+                <Form onSubmit={handleSubmit}>
+                    <h1>Faça seu cadastro</h1>
+                    <Input name="name" icon={FiUser} placeholder="Nome" />
+                    <Input name="email" icon={FiMail} placeholder="Email" />
+                    <Input
+                        name="password"
+                        icon={FiLock}
+                        type="password"
+                        placeholder="Senha"
+                    />
+                    <Button type="submit">Cadastrar</Button>
+                </Form>
+                <a href="login">
+                    <FiArrowLeft />
+                    Voltar para logon
+                </a>
+            </Content>
+        </Container>
+    );
+};
 
 export default SingUp;
