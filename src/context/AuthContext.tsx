908eddc84be6e19fedf029e4d7753174dbca9bac
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-08-18 23:51:11 -0300

    add: Keep User in LocalStorage

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useCallback } from 'react';
+/* eslint-disable @typescript-eslint/ban-types */
+import React, { createContext, useCallback, useState } from 'react';
 
 import api from '../services/api';
 
@@ -7,8 +8,13 @@ interface SignInCredentials {
     password: string;
 }
 
+interface AuthState {
+    token: string;
+    user: object;
+}
 interface AuthContextData {
-    name: string;
+    user: object;
+    // eslint-disable-next-line no-unused-vars
     signIn(credentials: SignInCredentials): Promise<void>;
 }
 
@@ -17,15 +23,33 @@ export const AuthContext = createContext<AuthContextData>(
 );
 
 export const AuthProvider: React.FC = ({ children }) => {
+    const [data, setData] = useState<AuthState>(() => {
+        const token = localStorage.getItem('@CompassOs:token');
+        const user = localStorage.getItem('@CompassOs:user');
+
+        if (token && user) {
+            return { token, user: JSON.parse(user) };
+        }
+
+        return {} as AuthState;
+    });
+
     const signIn = useCallback(async ({ email, password }) => {
         const response = await api.post('sessions', {
             email,
             password,
         });
+
+        const { token, user } = response.data;
+
+        localStorage.setItem('@CompassOs:token', token);
+        localStorage.setItem('@CompassOs:user', JSON.stringify(user));
+
+        setData({ token, user });
     }, []);
 
     return (
-        <AuthContext.Provider value={{ name: 'Murilo', signIn }}>
+        <AuthContext.Provider value={{ user: data.user, signIn }}>
             {children}
         </AuthContext.Provider>
     );
