760fc803df765d8daa3b703b5973e0297ce674a1
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-08-18 21:45:41 -0300

    add: Create Context

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,11 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
+import AuthContext from '../../context/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/compass-orange.png';
@@ -16,6 +17,8 @@ import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
+
+    const { name } = useContext(AuthContext);
 
     const handleSubmit = useCallback(async data => {
         try {
