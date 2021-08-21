efcf5aa9aa8ba600c4b81c5306081e7395e9cbd9
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-08-21 01:22:38 -0300

    add: Finished 1th Release

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,7 +3,7 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
@@ -24,6 +24,7 @@ interface SignInFormData {
 
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
+    const history = useHistory();
 
     const { signIn } = useAuth();
     const { addToast } = useToast();
@@ -47,6 +48,8 @@ const SignIn: React.FC = () => {
                     email: data.email,
                     password: data.password,
                 });
+
+                history.push('/dashboard');
             } catch (err) {
                 if (err instanceof Yup.ValidationError) {
                     const errors = getValidationErrors(err);
@@ -57,13 +60,13 @@ const SignIn: React.FC = () => {
                 }
                 addToast({
                     type: 'error',
-                    title: 'Erro na autenticaçã!',
+                    title: 'Erro na autenticação!',
                     description:
                         'Ocorreu um erro ao fazer login, cheque as credentials',
                 });
             }
         },
-        [signIn, addToast],
+        [signIn, addToast, history],
     );
 
     return (
