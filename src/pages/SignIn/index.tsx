b13bf71b77bc22671736cde49a5db21c5e748021
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-08-21 00:19:38 -0300

    add: Abination Form & Link Page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,9 @@
 import React, { useRef, useCallback } from 'react';
-
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
-import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
 
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
@@ -15,7 +15,7 @@ import logoImg from '../../assets/compass-orange.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
 
 interface SignInFormData {
     email: string;
@@ -52,6 +52,8 @@ const SignIn: React.FC = () => {
                     const errors = getValidationErrors(err);
 
                     formRef.current?.setErrors(errors);
+
+                    return;
                 }
                 addToast({
                     type: 'error',
@@ -67,24 +69,26 @@ const SignIn: React.FC = () => {
     return (
         <Container>
             <Content>
-                <img src={logoImg} alt="Logo Compass OS" />
-                <Form ref={formRef} onSubmit={handleSubmit}>
-                    <h1>Faça seu logon</h1>
-                    <Input name="email" icon={FiMail} placeholder="Email" />
-                    <Input
-                        name="password"
-                        icon={FiLock}
-                        type="password"
-                        placeholder="Senha"
-                    />
-                    <Button type="submit">Entrar</Button>
+                <AnimationContainer>
+                    <img src={logoImg} alt="Logo Compass OS" />
+                    <Form ref={formRef} onSubmit={handleSubmit}>
+                        <h1>Faça seu logon</h1>
+                        <Input name="email" icon={FiMail} placeholder="Email" />
+                        <Input
+                            name="password"
+                            icon={FiLock}
+                            type="password"
+                            placeholder="Senha"
+                        />
+                        <Button type="submit">Entrar</Button>
 
-                    <a href="forgot">Esqueci minha senha</a>
-                </Form>
-                <a href="login">
-                    <FiLogIn />
-                    Criar conta
-                </a>
+                        <a href="forgot">Esqueci minha senha</a>
+                    </Form>
+                    <Link to="/signup">
+                        <FiLogIn />
+                        Criar conta
+                    </Link>
+                </AnimationContainer>
             </Content>
             <Background />
         </Container>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,6 +3,8 @@ import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/compass-orange.png';
@@ -10,7 +12,7 @@ import logoImg from '../../assets/compass-orange.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
 
 const SingUp: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
@@ -39,23 +41,25 @@ const SingUp: React.FC = () => {
         <Container>
             <Background />
             <Content>
-                <img src={logoImg} alt="Logo Compass OS" />
-                <Form ref={formRef} onSubmit={handleSubmit}>
-                    <h1>Faça seu cadastro</h1>
-                    <Input name="name" icon={FiUser} placeholder="Nome" />
-                    <Input name="email" icon={FiMail} placeholder="Email" />
-                    <Input
-                        name="password"
-                        icon={FiLock}
-                        type="password"
-                        placeholder="Senha"
-                    />
-                    <Button type="submit">Cadastrar</Button>
-                </Form>
-                <a href="login">
-                    <FiArrowLeft />
-                    Voltar para logon
-                </a>
+                <AnimationContainer>
+                    <img src={logoImg} alt="Logo Compass OS" />
+                    <Form ref={formRef} onSubmit={handleSubmit}>
+                        <h1>Faça seu cadastro</h1>
+                        <Input name="name" icon={FiUser} placeholder="Nome" />
+                        <Input name="email" icon={FiMail} placeholder="Email" />
+                        <Input
+                            name="password"
+                            icon={FiLock}
+                            type="password"
+                            placeholder="Senha"
+                        />
+                        <Button type="submit">Cadastrar</Button>
+                    </Form>
+                    <Link to="/">
+                        <FiArrowLeft />
+                        Voltar para logon
+                    </Link>
+                </AnimationContainer>
             </Content>
         </Container>
     );
