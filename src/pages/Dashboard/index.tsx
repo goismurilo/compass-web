47e7c50b6e26d5309bf5f7ac0413b707142bfef7
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-08-21 00:57:32 -0300

    add: Authenticated Routes

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+    return (
+        <>
+            <h1>Faça</h1>
+            <h2>Parte</h2>
+        </>
+    );
+};
+
+export default Dashboard;
